abc304e21ff1d492f76b756239251c844d7f1c1e
Pasha Matveev <pasha.matveev.1287@mail.ru>
2025-02-21 17:47:16 +0300

    added contour detection

--- modules/camera.py
+++ modules/camera.py
@@ -11,16 +11,16 @@
 #         self.blue_goal_threshold = self.settings['camera']['objects'][2]['threshold']
 #         self.frame = None
 #         self.start()
-    
+
 #     def start(self):
 #         self.video = cv.VideoCapture(0)
-    
+
 #     def read(self):
 #         ret, self.frame = self.video.read()
-    
+
 #     def preview(self):
 #         pass
-    
+
 #     def stop(self):
 #         pass
 
@@ -41,7 +41,6 @@
 #     yuv420 = picam2.capture_array("lores")
 #     rgb = cv2.cvtColor(yuv420, cv2.COLOR_YUV420p2RGB)
 #     cv2.imshow("Camera", rgb)
-
 
 
 from __future__ import print_function
@@ -67,36 +66,47 @@ high_H_name = 'High H'
 high_S_name = 'High S'
 high_V_name = 'High V'
 
+
 def on_low_H_thresh_trackbar(val):
     global low_H
     global high_H
     low_H = val
     low_H = min(high_H-1, low_H)
     cv.setTrackbarPos(low_H_name, window_detection_name, low_H)
+
+
 def on_high_H_thresh_trackbar(val):
     global low_H
     global high_H
     high_H = val
     high_H = max(high_H, low_H+1)
     cv.setTrackbarPos(high_H_name, window_detection_name, high_H)
+
+
 def on_low_S_thresh_trackbar(val):
     global low_S
     global high_S
     low_S = val
     low_S = min(high_S-1, low_S)
     cv.setTrackbarPos(low_S_name, window_detection_name, low_S)
+
+
 def on_high_S_thresh_trackbar(val):
     global low_S
     global high_S
     high_S = val
     high_S = max(high_S, low_S+1)
     cv.setTrackbarPos(high_S_name, window_detection_name, high_S)
+
+
 def on_low_V_thresh_trackbar(val):
     global low_V
     global high_V
     low_V = val
     low_V = min(high_V-1, low_V)
     cv.setTrackbarPos(low_V_name, window_detection_name, low_V)
+
+
 def on_high_V_thresh_trackbar(val):
     global low_V
     global high_V
@@ -118,29 +128,46 @@ picam2.start()
 
 cv.namedWindow(window_capture_name)
 cv.namedWindow(window_detection_name)
-cv.createTrackbar(low_H_name, window_detection_name , low_H, max_value_H, on_low_H_thresh_trackbar)
-cv.createTrackbar(high_H_name, window_detection_name , high_H, max_value_H, on_high_H_thresh_trackbar)
-cv.createTrackbar(low_S_name, window_detection_name , low_S, max_value, on_low_S_thresh_trackbar)
-cv.createTrackbar(high_S_name, window_detection_name , high_S, max_value, on_high_S_thresh_trackbar)
-cv.createTrackbar(low_V_name, window_detection_name , low_V, max_value, on_low_V_thresh_trackbar)
-cv.createTrackbar(high_V_name, window_detection_name , high_V, max_value, on_high_V_thresh_trackbar)
+cv.createTrackbar(low_H_name, window_detection_name, low_H,
+                  max_value_H, on_low_H_thresh_trackbar)
+cv.createTrackbar(high_H_name, window_detection_name, high_H,
+                  max_value_H, on_high_H_thresh_trackbar)
+cv.createTrackbar(low_S_name, window_detection_name, low_S,
+                  max_value, on_low_S_thresh_trackbar)
+cv.createTrackbar(high_S_name, window_detection_name, high_S,
+                  max_value, on_high_S_thresh_trackbar)
+cv.createTrackbar(low_V_name, window_detection_name, low_V,
+                  max_value, on_low_V_thresh_trackbar)
+cv.createTrackbar(high_V_name, window_detection_name, high_V,
+                  max_value, on_high_V_thresh_trackbar)
 
 
 while True:
-    
+
     # ret, frame = cap.read()
     # if frame is None:
     #     break
 
     yuv420 = picam2.capture_array("lores")
-    frame = cv.cvtColor(yuv420, cv.COLOR_YUV420p2BGR)
-    frame_HSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    frame_threshold = cv.inRange(frame_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
-    
-    
+    frame = cv.cvtColor(yuv420, cv.COLOR_YUV420p2RGB)
+    frame_HSV = cv.cvtColor(frame, cv.COLOR_RGB2HSV)
+    frame_threshold = cv.inRange(
+        frame_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
+
+    contours, hierarchy = cv.findContours(frame_threshold, 1, 2)
+
+    if len(contours):
+        cnt = max(contours, key=lambda x: cv.contourArea(x))
+        M = cv.moments(cnt)
+
+        (x, y), radius = cv.minEnclosingCircle(cnt)
+        center = (int(x), int(y))
+        radius = int(radius)
+        cv.circle(frame, center, radius, (255, 0, 0), 2)
+
     cv.imshow(window_capture_name, frame)
     cv.imshow(window_detection_name, frame_threshold)
-    
+
     key = cv.waitKey(30)
     if key == ord('q') or key == 27:
         break

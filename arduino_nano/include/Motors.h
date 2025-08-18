87e2704f9901ef4657d9f64e23bcf3e4564f9f21
Roman Kolerov <coder8080@yandex.ru>
2025-08-18 12:11:16 +0300

    Fix build

--- arduino_nano/include/Motors.h
+++ arduino_nano/include/Motors.h
@@ -18,7 +18,7 @@ private:
 public:
   void init();
   void setpowers(int32_t *);
-  void run(float, float, float, float);
+  void run(float, float, float, float = -1);
   void stop();
   void requestVoltageData();
   float parseMotorVoltage();
@@ -72,7 +72,7 @@ void Motors::stop()
   }
 }
 
-void Motors::run(float angle, float speed, float rotation, float rot_limit = -1)
+void Motors::run(float angle, float speed, float rotation, float rot_limit)
 {
   rot_limit = rot_limit == -1 ? 30 : rot_limit;
   double angles[3] = {M_PI / 3.0, M_PI, M_PI * 5.0 / 3.0};

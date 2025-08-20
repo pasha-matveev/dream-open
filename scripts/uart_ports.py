ededd29764c464e860278f7d3dda38783299a45c
Roman Kolerov <coder8080@yandex.ru>
2025-08-20 17:03:46 +0300

    Fix some python issues #2

--- scripts/uart_ports.py
+++ scripts/uart_ports.py
@@ -2,5 +2,5 @@ import serial.tools.list_ports
 
 ports = serial.tools.list_ports.comports()
 for i, (port, desc, hwid) in enumerate(sorted(ports)):
-    print("{}. {}: {} [{}]".format(i+1, port, desc, hwid))
+    print("{}. {}: {} [{}]".format(i + 1, port, desc, hwid))
     print(desc)

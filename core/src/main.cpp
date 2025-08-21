75467ae70ccc433d0e6008c56b2dddd07483b168
Roman Kolerov <coder8080@yandex.ru>
2025-08-21 17:15:49 +0300

    Add camera preview

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <unistd.h>
 
-#include <opencv2/opencv.hpp>
+#include <chrono>
+#include <thread>
+
+#include "camera.h"
 
 using namespace cv;
 using namespace std;
 
-int main() {}
+int main() {
+    Camera camera;
+    camera.start();
+    while (true) {
+        camera.capture();
+        camera.preview();
+        if (cv::waitKey(1000 / VIDEO_FPS / 2) == 27) {  // 27 = ESC
+            break;
+        }
+    }
+}

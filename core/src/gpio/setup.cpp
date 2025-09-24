d0e224fabad664828847690af2e2b19fe5de095f
Roman Kolerov <coder8080@yandex.ru>
2025-09-24 08:46:04 +0000

    Fix camera

--- core/src/gpio/setup.cpp
+++ core/src/gpio/setup.cpp
@@ -1,5 +1,7 @@
 #include <wiringPi.h>
 
+using namespace std;
+
 void setup_wiringpi() {
     if (wiringPiSetupPinType(WPI_PIN_BCM) == -1) {
         throw runtime_error("Failed to setup wiringPi");

ef72ca68abe9fd00b9b90b7f75e63e36f84e86c5
Roman Kolerov <coder8080@yandex.ru>
2025-10-04 12:09:11 +0300

    Add visualization & Use Google formatting

--- core/include/robot.h
+++ core/include/robot.h
@@ -9,38 +9,38 @@
 using namespace LibSerial;
 
 class Robot {
-   private:
-    void init_camera();
-    void init_buzzer();
-    void init_buttons();
-    void init_uart();
-    void init_display();
-    void init_lidar();
+ private:
+  void init_camera();
+  void init_buzzer();
+  void init_buttons();
+  void init_uart();
+  void init_display();
+  void init_lidar();
 
-   public:
-    Camera *camera = nullptr;
-    Buzzer *buzzer = nullptr;
-    UART *uart = nullptr;
-    Display *display = nullptr;
-    Lidar *lidar = nullptr;
+ public:
+  Camera *camera = nullptr;
+  Buzzer *buzzer = nullptr;
+  UART *uart = nullptr;
+  Display *display = nullptr;
+  Lidar *lidar = nullptr;
 
-    Robot() = default;
-    ~Robot();
+  Robot() = default;
+  ~Robot();
 
-    float angle = 0;
-    bool emitter = false;
-    bool kicker_charged = false;
+  float angle = 0;
+  bool emitter = false;
+  bool kicker_charged = false;
 
-    float direction = 0;
-    float speed = 0;
-    float rotation = 0;
-    float rotation_limit = 30;
-    int dribling = 0;
-    int kicker_force = 0;
-    bool rgb_led = false;
+  float direction = 0;
+  float speed = 0;
+  float rotation = 0;
+  float rotation_limit = 30;
+  int dribling = 0;
+  int kicker_force = 0;
+  bool rgb_led = false;
 
-    void init_hardware();
+  void init_hardware();
 
-    void read_from_arduino();
-    void write_to_arduino();
+  void read_from_arduino();
+  void write_to_arduino();
 };

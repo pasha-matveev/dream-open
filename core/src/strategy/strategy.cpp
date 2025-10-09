7b914702be51a73e52ff51a80d94dba1cd455b06
Dream Black <romkolor@gmail.com>
2025-10-09 15:56:10 +0300

    Small strategy

--- core/src/strategy/strategy.cpp
+++ core/src/strategy/strategy.cpp
@@ -5,29 +5,30 @@
 #include "utils/vec.h"
 
 void Strategy::run(Robot &robot, Ball &ball) {
+  // cout << ball.get_cm() << endl;
   robot.compute_lidar();
 
-  cout << ball.get_pixels_dist() << endl;
-  cout << ball.relative_angle << endl;
-  cout << ball.get_cm() << endl;
-
-  // if (ball.visible) {
-  //   if (robot.emitter) {
-  //     robot.dribling = 0;
-  //     robot.rotation = 0;
-  //     robot.speed = 0;
-  //     robot.kicker_force = 20;
-  //   } else {
-  //     robot.dribling = 50;
-  //     robot.rotation = ball.relative_angle;
-  //     robot.rotation_limit = 50;
-  //     robot.speed = 80;
-  //     robot.direction = ball.relative_angle;
-  //     robot.kicker_force = 0;
-  //   }
-  // } else {
-  //   robot.rotation = 0;
-  //   robot.speed = 0;
-  //   robot.kicker_force = 0;
-  // }
+  if (ball.visible) {
+    if (ball.get_cm() < 8.2) {
+      if (abs(robot.field_angle) <= 0.1) {
+        robot.kicker_force = 10;
+      }
+      robot.rotation_limit = 10;
+      robot.dribling = 30;
+      robot.rotation = robot.field_angle * -1;
+      robot.speed = 0;
+      robot.kicker_force = 20;
+    } else {
+      robot.dribling = 30;
+      robot.rotation = ball.relative_angle;
+      robot.rotation_limit = 50;
+      robot.speed = 10;
+      robot.direction = ball.relative_angle;
+      robot.kicker_force = 0;
+    }
+  } else {
+    robot.rotation = 0;
+    robot.speed = 0;
+    robot.kicker_force = 0;
+  }
 }

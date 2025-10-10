4ae2142e4f1309ed520c32b28ee269634e12c8b5
Dream Black <romkolor@gmail.com>
2025-10-10 10:07:57 +0300

    Bump strategy

--- core/src/strategy/strategy.cpp
+++ core/src/strategy/strategy.cpp
@@ -8,28 +8,30 @@ void Strategy::run(Robot &robot, Ball &ball) {
   // cout << ball.get_cm() << endl;
   robot.compute_lidar();
 
-  if (ball.visible) {
-    if (robot.emitter) {
-      Vec target{91, 243};
-      Vec v = target - robot.position;
-      Vec robot_dir{-1 * sin(robot.field_angle), cos(robot.field_angle)};
-      double ang = atan2(robot_dir % v, robot_dir * v);
-      robot.rotation_limit = 10;
-      robot.dribling = 30;
-      robot.rotation = ang;
-      robot.speed = 0;
-      robot.kicker_force = 0;
-      if (abs(ang) <= 0.01) {
-        robot.kicker_force = 10;
-      }
-    } else {
-      robot.dribling = 30;
-      robot.rotation = ball.relative_angle;
-      robot.rotation_limit = 50;
-      robot.speed = 30;
-      robot.direction = ball.relative_angle;
-      robot.kicker_force = 0;
+  if (robot.emitter) {
+    Vec target{91, 243};
+    Vec v = target - robot.position;
+    Vec robot_dir{-1 * sin(robot.field_angle), cos(robot.field_angle)};
+    double ang = atan2(robot_dir % v, robot_dir * v);
+    robot.rotation_limit = 10;
+    robot.dribling = 40;
+    robot.rotation = ang;
+    robot.speed = 0;
+    robot.kicker_force = 0;
+    if (abs(ang) <= 0.05) {
+      robot.kicker_force = 30;
     }
+  } else if (ball.visible) {
+    robot.dribling = 40;
+    robot.rotation = ball.relative_angle;
+    robot.rotation_limit = 30;
+    if (ball.get_cm() > 10) {
+      robot.speed = 40;
+    } else {
+      robot.speed = 10;
+    }
+    robot.direction = ball.relative_angle;
+    robot.kicker_force = 0;
   } else {
     robot.rotation = 0;
     robot.speed = 0;

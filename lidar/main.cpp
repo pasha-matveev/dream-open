58045e3b05a0efdfd02c432ac24758ddb59b7042
Dream Black <romkolor@gmail.com>
2025-10-30 18:49:00 +0300

    Upgrades

--- lidar/main.cpp
+++ lidar/main.cpp
@@ -536,7 +536,7 @@ int main(int argc, const char* argv[]) {
     }
   };
 
-  for (int i = 1; i < 10 && !connectSuccess; ++i) {
+  for (int i = 0; i < 10 && !connectSuccess; ++i) {
     try_connect(i);
   }
 

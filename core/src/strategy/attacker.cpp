4b8c6e222050c996e466caf58154e71117ce4d8e
Roman Kolerov <coder8080@yandex.ru>
2025-11-07 12:04:46 +0300

    Update attacker logic

--- core/src/strategy/attacker.cpp
+++ core/src/strategy/attacker.cpp
@@ -4,26 +4,27 @@
 void Strategy::run_attacker(Robot& robot, Object& ball, Object& goal) {
   const int BORDER = 80;
 
-  if (millis() - last_ball_visible < 3000) {
-    if (robot.emitter) {
-      int power = 70;
-      if (robot.position.y < 146) {
-        power = 40;
-      } else if (robot.position.y < 180) {
-        power = 20;
-      } else {
-        power = 15;
-      }
-      hit(robot, goal, power = power);
+  if (robot.emitter) {
+    int power = 70;
+    if (robot.position.y < 146) {
+      power = 40;
+    } else if (robot.position.y < 180) {
+      power = 20;
     } else {
+      power = 15;
+    }
+    hit(robot, goal, power = power);
+  } else {
+    if (millis() - last_ball_visible < 3000) {
       if (last_ball.y < BORDER) {
         drive_target(robot, {160, 120}, 4);
       } else {
         drive_ball(robot, last_ball);
       }
+    } else {
+      drive_target(robot, {91, 121}, 3);
     }
-  } else {
-    drive_target(robot, {91, 121}, 3);
   }
+
   robot.vel = robot.vel.resize(min(robot.vel.len(), 120.0));
 }

6f72c25f9d4254c363817299d05bccbbe0eb7710
Dream Black <romkolor@gmail.com>
2025-11-12 14:43:07 +0300

    Lower movement precision

--- core/src/strategy/common/drive_target.cpp
+++ core/src/strategy/common/drive_target.cpp
@@ -4,7 +4,7 @@ bool Strategy::drive_target(Robot& robot, const Vec& target, double k,
                             double max_speed, double min_speed) {
   Vec vel = target - robot.position;
   robot.rotation = normalize_angle(vel.field_angle() - robot.field_angle);
-  bool finished = vel.len() <= 2.0;
+  bool finished = vel.len() <= 3.0;
   vel *= k;
   vel = vel.resize(clamp(vel.len(), min_speed, max_speed));
   robot.vel = vel;

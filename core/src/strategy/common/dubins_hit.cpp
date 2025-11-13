4c323031dd5b5da189eaf98a277462b880fea0f9
Dream Black <romkolor@gmail.com>
2025-11-13 10:10:52 +0300

    Dubins path v3

--- core/src/strategy/common/dubins_hit.cpp
+++ core/src/strategy/common/dubins_hit.cpp
@@ -4,15 +4,20 @@
 #include "utils/nmap.h"
 
 void Strategy::dubins_hit(Robot& robot, Object& goal, int power) {
-  if (robot.emitter) {
-    robot.kicker_force = power;
-    return;
-  }
+  robot.dribling = 40;
+
   reset_dubins = false;
   double dist = (last_ball - robot.position).len();
-  // TODO: dynamic target
-  Vec target{91, 240};
-  Vec dir = target - last_ball;
+  Vec dir;
+  if (goal.visible) {
+    double dir_angle = goal.relative_angle + robot.field_angle;
+    dir = Vec{dir_angle};
+  } else {
+    Vec target{91, 240};
+    // dir = target - last_ball;
+    dir = target - robot.position;
+  }
+
   Vec a = last_ball + dir.resize(-config.strategy.dubins.bonus);
   Vec left_center = a + dir.turn_left().resize(config.strategy.dubins.radius) +
                     dir.turn_right().resize(config.strategy.dubins.delta);
@@ -25,7 +30,7 @@ void Strategy::dubins_hit(Robot& robot, Object& goal, int power) {
   if ((last_ball - dubins_ball).len() > config.strategy.dubins.threshold) {
     dubins_side = DubinsSide::NONE;
   }
-  if (dubins_side == DubinsSide::NONE) {
+  if (true) {
     dubins_ball = last_ball;
     if (abs(left_circle.dist(robot.position)) <
         abs(right_circle.dist(robot.position))) {
@@ -45,7 +50,12 @@ void Strategy::dubins_hit(Robot& robot, Object& goal, int power) {
       dist, config.strategy.dubins.base_dist, config.strategy.dubins.max_dist,
       config.strategy.dubins.base_speed, config.strategy.dubins.max_speed);
   Vec vel;
-  if (abs(angle) <= 0.01) {
+  if (abs(angle) <= 0.2 && robot.emitter) {
+    robot.kicker_force = power;
+    reset_dubins = true;
+    return;
+  }
+  if (abs(angle) <= 0.05) {
     // Едем напрямую к мячу
     vel = last_ball - robot.position;
   } else if (circle.inside(robot.position) ||
@@ -74,5 +84,10 @@ void Strategy::dubins_hit(Robot& robot, Object& goal, int power) {
   }
   vel = vel.resize(speed);
   robot.vel = vel;
-  robot.rotation = last_ball_relative;
+  double target_relative =
+      normalize_angle(dir.field_angle() - robot.field_angle);
+  // robot.rotation =
+  //     normalize_angle(last_ball_relative / 2 + target_relative / 2);
+  robot.rotation = target_relative;
+  robot.rotation_limit = 30;
 }

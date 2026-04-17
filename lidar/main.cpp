9fefd8bfc2acd000197d20b03829ac0c83f5f0f4
Dream Black <romkolor@gmail.com>
2026-04-17 13:03:02 +0300

    Increase min cluster size

--- lidar/main.cpp
+++ lidar/main.cpp
@@ -690,7 +690,7 @@ int main(int argc, const char* argv[]) {
     partitionPointsInsideOutside(points, inner, inside, outside);
     vector<vector<Point>> clusters = clusterPoints(inside, 10);
     for (auto& cluster : clusters)
-      if (cluster.size() > 5)
+      if (cluster.size() > 6)
         result_data.push_back(minimalAreaBoundingRectangle(cluster));
 
     if (preview) {

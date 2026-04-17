9fba324449ca41e35842a9694e7c39de57bc8af0
Dream Black <romkolor@gmail.com>
2026-04-17 16:07:21 +0300

    Test points strategy

--- core/src/strategy/test.cpp
+++ core/src/strategy/test.cpp
@@ -18,4 +18,30 @@ void Strategy::run_test_dribling(Robot& robot) {
   robot.dribling = config.strategy.dribbling.value_r;
 }
 
-void Strategy::run_test(Robot& robot, Object& goal) {}
+static int state = 0;
+
+void Strategy::run_test(Robot& robot, Object& goal) {
+  const double PADDING = 70;
+
+  if (robot.emitter) {
+    vector<Vec> points = {{PADDING, PADDING},
+                          {182 - PADDING, PADDING},
+                          {182 - PADDING, 220 - PADDING},
+                          {PADDING, 220 - PADDING}};
+    vector<double> point_rotation = {M_PI / 2, M_PI, -M_PI / 2, 0};
+
+    auto handle = [&](Robot& robot, Vec& target) {
+      robot.rotation =
+          normalize_angle(point_rotation[state] - robot.field_angle);
+      robot.rotation_limit = 15;
+      drive_target(robot, target, 1, 80, 80);
+      if ((target - robot.position).len() <= 10) {
+        state = (state + 1) % 4;
+      }
+    };
+
+    handle(robot, points[state]);
+    robot.dribling = config.strategy.dribbling.value_r;
+  } else {
+  }
+}
